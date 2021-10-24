314c683d7cd498844e86ada7da8f3bc66f5f5132
Helmangit <helman.sanchez.mt@correo.usa.edu.co>
2021-10-24 16:47:48 -0500

    first commit

---
+++ src/test/java/Reto3_Motorb/Reto3_Motorb/Reto3MotorbApplicationTests.java
@@ -0,0 +1,16 @@
+package Reto3_Motorb.Reto3_Motorb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3MotorbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
